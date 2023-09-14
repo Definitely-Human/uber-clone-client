d232d184cdb7f8f4e790189257378f2b29273272
Definitely-Human <odest66@gmail.com>
2023-09-14 23:23:07 +0200

    Login page UI.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,15 @@
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <div className="h-screen flex items-center justify-center bg-gray-800">
+            <div className="bg-white w-full max-w-lg pt-7 pb-7 rounded-lg text-center">
+                <h3 className=" text-2xl text-gray-800">Log In</h3>
+                <form className="flex flex-col my-5 px-5">
+                    <input placeholder="Email" className="mb-3 input" />
+                    <input placeholder="Password" className="input" />
+                    <button className="mt-3 btn">Log In</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 export default Login;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "../pages/login";
+import Login from "../pages/Login";
 import CreateAccount from "../pages/CreateAccount";
 
 const LoggedOutRouter = () => {
