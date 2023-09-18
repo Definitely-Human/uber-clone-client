6a35235c4dd5f1476105d4b2c7dea260ff50a21f
Definitely-Human <odest66@gmail.com>
2023-09-19 00:05:39 +0200

    Added category ui.

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -42,6 +42,37 @@ const Restaurants = () => {
         },
     });
     console.log(data, error);
-    return <h1>Restaurants</h1>;
+    return (
+        <div>
+            <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+                <input
+                    type="search"
+                    className="input w-3/12 rounded-md border-0"
+                    placeholder="Search restaurant..."
+                />
+            </form>
+            {!loading && (
+                <div className="max-w-screen-2xl mx-auto mt-8">
+                    <div className="flex justify-center max-w-sm mx-auto">
+                        {data?.allCategories.categories?.map((category) => (
+                            <div className="flex flex-col items-center">
+                                <div
+                                    key={category.id}
+                                    className="w-14 h-14 mx-6 cursor-pointer bg-cover hover:bg-gray-200 rounded-full"
+                                    style={{
+                                        backgroundImage: `url(${category.coverImage})`,
+                                    }}
+                                ></div>
+                                <span className="text-sm mt-1 text-center font-bold">
+                                    {category.name}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+            <div></div>
+        </div>
+    );
 };
 export default Restaurants;
